cc37781d93203e5e1d8dd30f4382194119c6840c
Radhika-Saini <59209590+Radhika-Saini@users.noreply.github.com>
2020-07-17 13:37:05 +0530

    Updates

---
+++ precache-manifest.62f2a0efd7a6e467e474e02871f83ed8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27d6b0123b61f8b67ad78233114ff77a",
+    "url": "/calorie_fusion/index.html"
+  },
+  {
+    "revision": "8cbe684a30ee33f66bd0",
+    "url": "/calorie_fusion/static/css/main.9bdaa4ee.chunk.css"
+  },
+  {
+    "revision": "2549930c7ca21a534508",
+    "url": "/calorie_fusion/static/js/2.6dae5892.chunk.js"
+  },
+  {
+    "revision": "8cbe684a30ee33f66bd0",
+    "url": "/calorie_fusion/static/js/main.6f70902d.chunk.js"
+  },
+  {
+    "revision": "f15221759e451814256d",
+    "url": "/calorie_fusion/static/js/runtime~main.c560b989.js"
+  },
+  {
+    "revision": "51be1e212cc2bf6e96390055c04165e4",
+    "url": "/calorie_fusion/static/media/gain-weight.51be1e21.jpg"
+  },
+  {
+    "revision": "2096ac96713d1b9ea800bb4a8658a620",
+    "url": "/calorie_fusion/static/media/lose-weight.2096ac96.jpg"
+  },
+  {
+    "revision": "bbf49ea1366e24c0e3b43e7b4453540f",
+    "url": "/calorie_fusion/static/media/maintain-weight.bbf49ea1.jpg"
+  },
+  {
+    "revision": "1c904cc87b34a009fe3045870364ef46",
+    "url": "/calorie_fusion/static/media/masthead-min.1c904cc8.jpg"
+  },
+  {
+    "revision": "9390aa1033f467f1ae91dbe5b88d8467",
+    "url": "/calorie_fusion/static/media/welcome-food-min.9390aa10.jpg"
+  }
+]);
